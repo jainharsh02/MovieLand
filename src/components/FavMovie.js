9b11005d8a1ffe3115560f6f8f0f879d66df8055
jainharsh02 <jainharsh6247@gmail.com>
2024-06-04 04:12:01 +0530

    update1

--- src/components/FavMovie.js
+++ src/components/FavMovie.js
@@ -2,19 +2,12 @@ import db from "../firebase/config";
 
 import React, { useState, useEffect } from "react";
 import { doc, getDoc } from "firebase/firestore"; 
-// import { db } from "./firebase"; // Adjust the import based on your project structure
-import FavouriteMovies from "./FavouriteMovies"; // Adjust the import based on your project structure
+import FavouriteMovies from "./FavouriteMovies";
 
 const FavMovie = () => {
   const [favmovies, setFavMovies] = useState([]);
-
-  // Fetch movies from Firestore
   const fetchMovies = async () => {
-<<<<<<< HEAD
     const docRef = doc(db, "users", "harsh");
-=======
-    const docRef = doc(db, "Fav", "harsh");
->>>>>>> ab527f5d117a5eab21d86dec236b04925c92f3dc
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
